f8e6f77a7cd0cbb717b274b73585a06b7d4e95fc
caroline opiyo <opiyocaroline20@gmail.com>
2023-12-08 22:27:09 +0300

    Working with Seed Data

--- lib/debug.py
+++ lib/debug.py
@@ -5,15 +5,18 @@ import random
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import Game
+from models import Game,Base
 
 fake = Faker()
 
 if __name__ == '__main__':
     
     engine = create_engine('sqlite:///seed_db.db')
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
+    
+    
 
 
     import ipdb; ipdb.set_trace()
--- lib/seed.py
+++ lib/seed.py
@@ -5,12 +5,37 @@ import random
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import Game
+from models import Game,Base
+fake=Faker()
 
 fake = Faker()
 
 if __name__ == '__main__':
+    try:
+        
+        engine = create_engine('sqlite:///seed_db.db')
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        session.query(Game).delete()
+        games=[
+            Game(
+                title=fake.name(),
+                genre=fake.word(),
+                platform=fake.word(),
+                price=fake.random.randint(0,50)
+
+            )
+            for i in range(50)]
+       
+        session.bulk_save_objects(games)
+     
+        session.commit()
+    except Exception as e:
+        print("encounted this error", str(e))    
+
+     
+     
     
-    engine = create_engine('sqlite:///seed_db.db')
-    Session = sessionmaker(bind=engine)
-    session = Session()
+
+   
